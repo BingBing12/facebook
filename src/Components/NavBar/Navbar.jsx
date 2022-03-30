e33c3bcd008e8b9ee1794a0df4ba0a7ff79191ac
Robin Tainton <robintainton0509@gmail.com>
2022-03-31 07:50:50 +0900

    navbar logo and search

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -2,15 +2,26 @@ import React from "react";
 import Flex_start from "../Flex/Flex_start";
 import Flex_mid from "../Flex/Flex_mid";
 import Flex_end from "../Flex/Flex_end";
+import Search from "../input/Search";
 import styles from "./navbar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  solid,
+  regular,
+  brands,
+} from "@fortawesome/fontawesome-svg-core/import.macro";
 
 const Navbar = () => {
   return (
     <Flex_start>
       <div className={styles.navbar}>
         <Flex_start>
-          <div>logo</div>
-          <div>search bar</div>
+          <div className={styles.logo}>
+            <FontAwesomeIcon size="2x" icon={brands("facebook")} />
+          </div>
+          <div className={styles.search}>
+            <Search />
+          </div>
         </Flex_start>
       </div>
       <div className={styles.navbar_mid}>
---
+++ src/Components/input/Search.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./search.module.css";
+
+const Search = (props) => {
+  return <input className={styles.search}></input>;
+};
+export default Search;
